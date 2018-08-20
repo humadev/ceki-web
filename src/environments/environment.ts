0108ebf70c0465d9c4075b748386c1c401b7c543
Huma Prathama <huma.elektro@gmail.com>
2018-08-21 06:19:52 +0800

    fix envi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  websocket: 'ws://128.199.196.171:4000',
+  websocket: 'ws://localhost:3000',
   endpoint: 'localhost',
   port: 3000,
   firebase: {
